f2bc46fd8ae358abfb4e4a6e1f664aed3de7bc42
Umar Baloch <umarbaloch262@gmail.com>
2023-12-25 03:36:04 +0500

    understanding syntax and numbers

---
+++ syntax.py
@@ -0,0 +1,10 @@
+#this is a comment in python 
+name = 'umar' #this is a inline comment
+
+#formats
+n = 200000
+print(f'this is a number with 2 decimals {n:,.2f}')
+
+#percentage
+population = 0.8
+print(f'over {population:.0%} population in US is drug addict')
