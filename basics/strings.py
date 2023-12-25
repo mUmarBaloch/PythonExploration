d06f7b96154ed780d7f9cc3315c05410bb62d20d
Umar Baloch <umarbaloch262@gmail.com>
2023-12-26 04:35:51 +0500

    string practice

---
+++ basics/strings.py
@@ -0,0 +1,16 @@
+greet = 'Hi! '
+greet = greet * 5
+
+print(greet)
+name = 'Sara'
+greeting_sentence =  f'Hi! I am {name}, your personal assistant'
+ord = ord('s')
+char = chr(ord)
+
+print(name not in greeting_sentence)
+
+print(f'this will return ascii number of character \s {ord}')
+print(f'this will return character at ascii code {ord} which is {char}')
+
+
+print(f'length of greet variable is {len(greet)} and its type is {type(greet)}')
