7fb5a05364f12981c92b98273d455eece5c6f547
Umar Baloch <umarbaloch262@gmail.com>
2023-12-28 06:54:03 +0500

    loops and conditions

---
+++ basics/intermediate.py
@@ -0,0 +1,20 @@
+x,y =30,40
+#short hand if else
+print('x is greater') if x > y else print('y is greater')
+#short hand if
+if 0==0 : print('true condition')
+a = 330
+b = 380
+print("A") if a > b else print("=") if a == b else print("B")
+
+#and or not , pass
+
+if 1>2 or 2==2:
+    if 2==2 and 3==3:
+            pass
+else :
+    print('false')
+
+# loops while and for is already discussed!!
+    
+# functions
